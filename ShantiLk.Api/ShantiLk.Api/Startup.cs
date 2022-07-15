28360395ae8462d95489bbbe2315ab61928db9d7
Anton Belov <63907966+Lichuhah@users.noreply.github.com>
2022-07-15 04:32:58 +0300

    Project created and configured

---
+++ ShantiLk.Api/ShantiLk.Api/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace ShantiLk.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt => { opt.EnableEndpointRouting = false; opt.Filters.Add(new AuthorizeFilter()); }).AddNewtonsoftJson();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+                    options.Cookie.Name = "SimpleTalk.AuthCookieAspNetCore";
+                    options.LoginPath = "/Auth/Login";
+                    options.LogoutPath = "/Auth/Logout";
+                });
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+                options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.None;
+                options.Secure = CookieSecurePolicy.None;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMvc(opt =>
+            {
+                opt.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{codigo?}");
+            });
+        }
+    }
+}
